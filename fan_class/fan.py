405a5a2170022c1fb86166586de7b417bbe1f31e
Kryzle Camille <kryzlesayat@gmail.com>
2026-05-31 01:49:53 +0800

    created private data field with data types as per instructions

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -2,3 +2,9 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def __init__(self, speed, on, radius, color):
+        self.__speed = int(speed)
+        self.__on = bool(on)
+        self.__radius = float(radius)
+        self.__color = str(color)
