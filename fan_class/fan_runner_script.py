70d0e2a6e7f9d2db1c96cdb33ab60d84fea81f7d
Kryzle Camille <kryzlesayat@gmail.com>
2026-05-31 02:25:57 +0800

    tried test program for the needed two fan objects and prints their results

--- fan_class/fan_runner_script.py
+++ fan_class/fan_runner_script.py
@@ -1,5 +1,7 @@
 from fan import Fan
 
-fan_one = Fan(Fan.SLOW, False, 67, "red")
+fan_one = Fan(Fan.FAST, True, 10, "yellow")
+fan_two = Fan(Fan.MEDIUM)
 
 print(f"Fan One: {fan_one.get_speed()}, {fan_one.get_on()}, {fan_one.get_radius()}, {fan_one.get_color()}")
+print(f"Fan Two: {fan_two.get_speed()}, {fan_two.get_on()}, {fan_two.get_radius()}, {fan_two.get_color()}")
