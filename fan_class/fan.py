6e8ce57cebeae2bdd8d9b26bc2a132fc448096d4
Kryzle Camille <kryzlesayat@gmail.com>
2026-05-31 01:52:44 +0800

    added the accessor and mutator methods as per 6th instruction

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -8,3 +8,23 @@ class Fan:
         self.__on = bool(on)
         self.__radius = float(radius)
         self.__color = str(color)
+
+    def get_speed(self):
+        return self.__speed
+    def set_speed(self, speed):
+        self.__speed = int(speed)
+
+    def get_on(self):
+        return self.__on
+    def set_on(self, on):
+        self.__on = bool(on)
+
+    def get_radius(self):
+        return self.__radius
+    def set_radius(self, radius):
+        self.__radius = float(radius)
+
+    def get_color(self):
+        return self.__color
+    def set_color(self, color):
+        self.__color = color
