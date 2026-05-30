f86f4bfcc0eaa0cba9a00ef94016e6120c6bbdde
Kryzle Camille <kryzlesayat@gmail.com>
2026-05-31 02:53:49 +0800

    methods initial creation

--- car_class/car.py
+++ car_class/car.py
@@ -3,3 +3,12 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+
+    def accelerate(self)
+        pass
+
+    def brake(self)
+        pass
+    
+    def get_speed(self):
+        pass
