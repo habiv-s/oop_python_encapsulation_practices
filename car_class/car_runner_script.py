0e607b0e573079df29d1d6ef731d0a2ff41c15c2
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-01 23:07:39 +0800

    added loop to call acceleration 5 times, then call brake 5 times, also print after each call

--- car_class/car_runner_script.py
+++ car_class/car_runner_script.py
@@ -4,9 +4,11 @@ Car = Car(2026, "Aston Martin Valkyrie")
 print(f"My car is {Car._Car__year_model} {Car._Car__make}.")
 
 print("ACCELERATING...")
-Car.accelerate()
-print(f"Current Speed: {Car.get_speed()}")
+for i in range(5):
+    Car.accelerate()
+    print(f"Current Speed: {Car.get_speed()}")
 
 print("BRAKING...")
-Car.brake()
-print(f"Current Speed: {Car.get_speed()}")
+for i in range(5):
+    Car.brake()
+    print(f"Current Speed: {Car.get_speed()}")
