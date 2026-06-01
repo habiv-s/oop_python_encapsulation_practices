3dfea7e1f1c88fa2f62a889585be4f22baa37ae6
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-01 23:00:18 +0800

    testing brake method and prints

--- car_class/car_runner_script.py
+++ car_class/car_runner_script.py
@@ -3,5 +3,10 @@ from car import Car
 my_car = Car(2026, "Aston Martin Valkyrie")
 print(f"My car is {my_car._Car__year_model} {my_car._Car__make}.")
 
+print("ACCELERATING...")
 my_car.accelerate()
+print(f"Current Speed: {my_car.get_speed()}")
+
+print("BRAKING...")
+my_car.brake()
 print(f"Current Speed: {my_car.get_speed()}")
