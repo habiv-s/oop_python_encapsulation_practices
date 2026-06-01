a999ec1946c782e3941b58c53c7869038e5449f9
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-01 23:58:28 +0800

    added logic to ensure speed does not drop below 0

--- car_class/car.py
+++ car_class/car.py
@@ -8,7 +8,10 @@ class Car:
         self.__speed += 5
 
     def brake(self):
-        self.__speed -= 5
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
 
     def get_speed(self):
         return self.__speed
