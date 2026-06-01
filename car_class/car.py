75c6271ef442e866dfbda24f9b686ebbc0066868
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-02 00:41:18 +0800

    added new method for ers and aerodynamics of an AM Valkyrie

--- car_class/car.py
+++ car_class/car.py
@@ -15,3 +15,23 @@ class Car:
 
     def get_speed(self):
         return self.__speed
+
+    def ers_deployment(self):
+        """Simulates the Valkyrie's F1-style Energy Recovery System (ERS)
+           hybrid battery output based on your acceleration speed."""
+        if self.__speed == 0:
+            return "ERS - Idle [0 kW]"
+        elif self.__speed < 15:
+            return "ERS - Charging... [+40 kW]"
+        else:
+            return "ERS - Deploying Kinetic Boost! [-120 kW]"
+
+    def active_aero(self):
+        """Mimics Adrian Newey's active aerodynamics. The Valkyrie's
+           wings adjust dynamically depending on how fast you loop it."""
+        if self.__speed == 0:
+            return "REAR WING - STOWED (0° Alpha)"
+        elif self.__speed <= 15:
+            return "ACTIVE AERO - LOW DRAG MODE"
+        else:
+            return "ACTIVE AERO - HIGH DOWNFORCE MODE [VENTURI OPEN]"
