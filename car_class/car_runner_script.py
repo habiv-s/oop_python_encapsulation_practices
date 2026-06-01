d63fbda5fc39e8d70a8a99156e894d87d87b0383
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-01 22:57:53 +0800

    testing accelerate method and prints

--- car_class/car_runner_script.py
+++ car_class/car_runner_script.py
@@ -2,3 +2,6 @@ from car import Car
 
 my_car = Car(2026, "Aston Martin Valkyrie")
 print(f"My car is {my_car._Car__year_model} {my_car._Car__make}.")
+
+my_car.accelerate()
+print(f"Current Speed: {my_car.get_speed()}")
