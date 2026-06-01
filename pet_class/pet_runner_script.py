f06ec36309745dc84ed5fda95c66b4004992c8a3
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-02 01:58:44 +0800

    fixed output display for the new method and added colors

--- pet_class/pet_runner_script.py
+++ pet_class/pet_runner_script.py
@@ -2,17 +2,23 @@ from pet import Pet
 
 the_pet = Pet("", "", 0)
 
-pet_name_input = input("Enter a pet name: ")
+pet_name_input = input("\033[31mEnter a pet name:\033[0m ")
 the_pet.set_name(pet_name_input)
 
-pet_animal_type_input = input("Enter your pet animal type: ")
+pet_animal_type_input = input("\033[31mEnter your pet animal type:\033[0m ")
 the_pet.set_animal_type(pet_animal_type_input)
 
-pet_age_input = input("Enter your pet age: ")
+pet_age_input = input("\033[31mEnter your pet age:\033[0m ")
 the_pet.set_age(pet_age_input)
 
-print(f"""PET DATA
-Name: {the_pet.get_name()}
-Type: {the_pet.get_animal_type()}
-Age: {the_pet.get_age()}
-""")
+print(f"""
+======================
+   MEET YOUR PET!!!
+\033[33m{the_pet.pet_image()}\033[m
+======================
+      \033[32mPET DATA\033[0m
+======================
+\033[33mName:\033[0m {the_pet.get_name()}
+\033[33mType:\033[0m {the_pet.get_animal_type()}
+\033[33mAge:\033[0m {the_pet.get_age()}
+======================""")
