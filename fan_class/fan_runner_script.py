520282c03cd54683699420f3227beee4ee8620bd
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-02 00:53:57 +0800

    fixed display for better output

--- fan_class/fan_runner_script.py
+++ fan_class/fan_runner_script.py
@@ -3,5 +3,22 @@ from fan import Fan
 fan_one = Fan(Fan.FAST, True, 10, "yellow")
 fan_two = Fan(Fan.MEDIUM)
 
-print(f"Fan One: {fan_one.get_speed()}, {fan_one.get_on()}, {fan_one.get_radius()}, {fan_one.get_color()}")
-print(f"Fan Two: {fan_two.get_speed()}, {fan_two.get_on()}, {fan_two.get_radius()}, {fan_two.get_color()}")
+print(f"""------------------
+     FAN ONE
+------------------
+Speed: {fan_one.get_speed()}
+Status: {fan_one.get_on()}
+Radius: {fan_one.get_radius()}
+Color: {fan_one.get_color()}
+------------------""")
+
+print("\n")
+
+print(f"""------------------
+     FAN TWO
+------------------
+Speed: {fan_two.get_speed()}
+Status: {fan_two.get_on()}
+Radies: {fan_two.get_radius()}
+Color: {fan_two.get_color()}
+------------------""")
