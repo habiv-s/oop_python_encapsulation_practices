a835b7fcd2d51ad6ca0b6a3ee87e16010bbe4fa5
Kryzle Camille <kryzlesayat@gmail.com>
2026-06-02 00:47:00 +0800

    added color to print

--- car_class/car_runner_script.py
+++ car_class/car_runner_script.py
@@ -1,7 +1,7 @@
 from car import Car
 
 Car = Car(2026, "Aston Martin Valkyrie")
-print(f"My car is {Car._Car__year_model} {Car._Car__make}.")
+print(f"\033[93mMy car is {Car._Car__year_model} {Car._Car__make}.\033[0m")
 print("\033[94m" + r"""                                  ____________________                       
                           ______//                   \\\______              
                      ____/      /  [ AM VALKYRIE ]    \       \____         
@@ -16,21 +16,21 @@ print("\033[94m" + r"""                                  ____________________
 
 print("\n")
 
-print("ACCELERATING...")
+print("\033[92mACCELERATING...\033[0m")
 print("-" * 60)
 for i in range(1, 6):
     Car.accelerate()
-    print(f"""Acceleration {i} - Current Speed: {Car.get_speed()}
+    print(f"""\033[92mAcceleration {i} - Current Speed: {Car.get_speed()} mph\033[0m
 Hybrid State: {Car.ers_deployment()}
 Aero Wing: {Car.active_aero()}""")
     print("-" * 60)
 
 print("\n")
 
-print("BRAKING...")
+print("\033[91mBRAKING...\033[0m")
 for i in range(1, 6):
     Car.brake()
-    print(f"""Brake {i} - Current Speed: {Car.get_speed()}
+    print(f"""\033[91mBrake {i} - Current Speed: {Car.get_speed()} mph\033[0m
 Hybrid State: {Car.ers_deployment()}
 Aero Wing: {Car.active_aero()}""")
     print("-" * 60)
